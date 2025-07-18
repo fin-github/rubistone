63f4405ee0059701c0e0d3b24484ed264dfcba68
Fin M <fin@mace.law>
2025-07-18 14:01:01 -0400

    add module loader

--- runme.py
+++ runme.py
@@ -1,4 +1,16 @@
 import interactions as i
+from os import listdir
+from icecream import ic
 
 intents = i.Intents.DEFAULT
 bot = i.Client(intents=intents)
+
+for filename in listdir("modules"):
+    filename = filename.removesuffix(".py") # remove the .py from <modulename>.py
+    
+    importstatement = "modules." + filename
+    
+    try:
+        bot.load_extension(importstatement)
+    except Exception as e:
+        ic("ERROR WHILE LOADING EXTENSION", importstatement, "ERROR:", e)
